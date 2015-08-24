0cdc54f785d4d8181b099175dec766c133d0d129
Scott <syhlion@gmail.com>
2015-08-24 12:13:13 +0800

    修改排版

--- router.go
+++ router.go
@@ -7,11 +7,12 @@ import (
 func Router() (router *mux.Router) {
 	router = mux.NewRouter()
 
+	/****************** client use *************************/
+
 	// ws handshake
 	router.HandleFunc("/ws/{app_key}/{user_tag}", WSHandler).Methods("GET")
 
-	//push message api
-	router.HandleFunc("/api/push/{app_key}", PushHandler).Methods("POST")
+	/****************** Super admin use *************************/
 
 	//register user
 	router.HandleFunc("/api/register", RegisterHandler).Methods("POST")
@@ -21,6 +22,11 @@ func Router() (router *mux.Router) {
 
 	//list app
 	router.HandleFunc("/api/app-list", AppListHandler).Methods("GET")
+
+	/****************** admin use *************************/
+
+	//push message api
+	router.HandleFunc("/api/push/{app_key}", PushHandler).Methods("POST")
 
 	//list how many client
 	router.HandleFunc("/api/{app_key}/listonlineuser", ListClientHandler).Methods("GET")
